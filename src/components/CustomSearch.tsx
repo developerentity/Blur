31346e46d261f1a652a90070619c536815f0e9d3
Bohdan Kryzhnii <bohdan.kryzhnii@appvales.com>
2022-08-04 12:13:31 +0300

    refactor: separate Search input

---
+++ src/components/CustomSearch.tsx
@@ -0,0 +1,98 @@
+import { Box, alpha, InputBase, Typography } from "@mui/material"
+import { styled } from '@mui/material/styles';
+import { useEffect, useState } from "react";
+import { Icon } from '@iconify/react';
+
+const Search = styled("div")(({ theme }) => ({
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+        marginLeft: theme.spacing(1),
+        width: "auto",
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
+}));
+
+const CustomSearch = ({
+    setValue,
+    value,
+    title,
+    reset,
+    timeout = 700,
+    placeholder = 'Search'
+}: {
+    setValue: (str: string) => void,
+    value: string,
+    title?: string,
+    reset?: boolean,
+    timeout?: number,
+    placeholder?: string,
+}) => {
+    const [query, setQuery] = useState(value);
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setValue(query);
+        }, timeout);
+        return () => clearTimeout(timeoutId);
+    }, [setValue, timeout, query]);
+
+    // const onReset = () => {
+    //     setValue('');
+    //     setQuery('');
+    // };
+
+    return (
+        <>
+            {title ? <Typography variant="h5">{title}</Typography> : null}
+            <Search>
+                <SearchIconWrapper>
+                    <Box
+                        component={Icon}
+                        icon="line-md:search"
+                        sx={{ color: 'text.disabled', width: 25, height: 25 }}
+                    />
+                </SearchIconWrapper>
+                <StyledInputBase
+                    placeholder={`${placeholder}…`}
+                    onChange={(e) => setQuery(e.target.value)}
+                    value={query}
+                    inputProps={{ 'aria-label': 'search' }}
+                />
+            </Search>
+        </>
+    );
+};
+
+export default CustomSearch;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,57 +1,14 @@
 import {
   AppBar,
   IconButton,
-  styled,
   Toolbar,
   Typography,
-  alpha,
-  InputBase,
   Badge,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
-  },
-}));
+import CustomSearch from "./CustomSearch";
+import { useState } from "react";
 
 const Header = ({
   handleNavOpen,
@@ -62,6 +19,9 @@ const Header = ({
   handleCart: () => void,
   orderLength?: number
 }) => {
+
+  const [value, setValue] = useState('')
+
   return (
     <AppBar>
       <Toolbar>
@@ -76,16 +36,7 @@ const Header = ({
         >
           BLUR
         </Typography>
-        <Search>
-          <SearchIconWrapper>
-            <SearchIcon />
-          </SearchIconWrapper>
-          <StyledInputBase
-            placeholder="Search…"
-            inputProps={{ 'aria-label': 'search' }}
-          />
-        </Search>
-
+        <CustomSearch value={value} setValue={setValue} />
         <IconButton onClick={handleCart} sx={{ ml: 2 }}>
           <Badge
             color='warning'
