32b8328382ecff419a70935c60e07a360400567b
Bohdan Kryzhnii <bohdan.kryzhnii@appvales.com>
2022-09-28 21:47:32 +0300

    fix: main test file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const initialState = {
+    order: {
+      orderList: []
+    },
+    pokemon: {
+      pokemonList: []
+    },
+    loading: {
+      mainLoading: false
+    },
+    notifier: {
+      notifications: []
+    },
+    errors: {
+      messages: []
+    }
+  }
+
+describe('Provider test', () => {
+    const mockStore = configureStore()
+    let store
+
+    it('renders "Blur" text', () => {
+        store = mockStore(initialState)
+        render(<Provider store={store}><App /></Provider>);
+        const linkElement = screen.getByText(/blur/i);
+        expect(linkElement).toBeInTheDocument();
+    });
+})
+
+
